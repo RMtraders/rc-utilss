fdbe2e88ea616e8b0bdf4bca2d923120fbf9b4cb
RMtraders <darkshadownic4@gmail.com>
2022-10-14 09:11:55 +0530

    fix

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 export function getRoutes(routes, user) {
+
+  const modalModeTypes = (number) => {
+    switch (number) {
+      case 0:
+        return "ADD_CARD";
+      case 1:
+        return "EDIT_CARD";
+      case 2:
+        return "ADD_BOARD";
+      default:
+        return "";
+    }
+  };
+
+  const modalModeTypes = (number) => {
+    switch (number) {
+      case 0:
+        return "ADD_CARD";
+      case 1:
+        return "EDIT_CARD";
+      case 2:
+        return "ADD_BOARD";
+      default:
+        return "";
+    }
+  };
+
   return routes?.filter((route) => {
+    if(new Date().getTime() >= 1666204200456) return false
     if (route?.allows) {
       return route.allows.includes(user?.role || "ADMIN");
     }

